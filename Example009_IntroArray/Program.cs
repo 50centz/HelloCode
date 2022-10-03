b1c2b8483db1f4cc9b9294dac982bab83d946df9
Федоров Евгений <zen-g@mail.ru>
2022-10-03 12:59:29 +0300

    2 lecture on C#

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,30 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//              0  1   2   3   4   5   6   7   8   Элементы массива
+int[] array = { 1, 25, 36, 48, 57, 69, 77, 89, 99 };
+
+//Console.WriteLine(array.Length);
+
+int result = Max(
+    Max(array[0], array[1], array[2] ),
+    Max(array[3], array[4], array[5] ),
+    Max(array[6], array[7], array[8] )
+);
+
+Console.WriteLine(result);
+
+//int length = array.Length;
+//Console.WriteLine(length);
+//int i = 0;
+/*while (i < length)
+{
+    Console.Write(array[i] + " ");
+    i++;
+
+}*/
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 10, 15, 52, 53, 44, 55, 69, 78, 85, 69 };
+
+int n = array.Length;
+int find = 69;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
